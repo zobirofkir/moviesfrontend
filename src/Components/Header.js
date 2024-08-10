ad72104876d0683182e86572583640d27a635c49
zobirofkir <zobirofkir19@gmail.com>
2024-08-10 16:55:48 +0100

    Update Some Needs From NavbArd Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Logo from '../Images/logo.png';
-import axios from 'axios';
-import SearchResults from '../Screens/SearchResults';
+import React, { useState, useEffect } from 'react'
+import Logo from '../Images/logo.png'
+import axios from 'axios'
+import SearchResults from '../Screens/SearchResults'
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [state, setState] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
@@ -27,10 +27,6 @@ const Header = () => {
     fetchData(searchQuery);
   };
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   useEffect(() => {
     if (searchQuery) {
       fetchData(searchQuery);
@@ -49,42 +45,49 @@ const Header = () => {
     }
   }, [searchResults, searchQuery]);
 
+  const dropDown = () => {
+    setState(!state);
+  }
+
   return (
     <div>
-      <nav className="bg-black border-b border-gray-800">
-        <div className="max-w-screen-xl mx-auto flex items-center justify-between p-4">
-          <a href="/" className="flex items-center space-x-3">
-            <img src={Logo} className="h-8" alt="Logo" />
-            <span className="text-2xl font-semibold text-white">CSW-MOVIES</span>
+      <nav class="bg-black border-gray-200 dark:bg-black">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src={Logo} class="h-8" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CSW-MOVIES</span>
           </a>
-          <button 
-            onClick={toggleMenu} 
-            type="button" 
-            className="inline-flex items-center p-2 text-gray-500 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" 
-            aria-controls="navbar-default" 
-            aria-expanded={menuOpen}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
+          <button data-collapse-toggle="navbar-default" onClick={dropDown} type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
           </button>
-          <div className={`${menuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto`} id="navbar-default">
-            <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:py-0 bg-black md:bg-transparent">
-              <li><a href="/" className="text-white hover:bg-blue-600 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a></li>
-              <li><a href="tv" className="text-gray-400 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">TV</a></li>
-              <li><a href="films" className="text-gray-400 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Film</a></li>
-              <li><a href="contacts" className="text-gray-400 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Contact</a></li>
+          <div className={`${state ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+
+              <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
               <li>
-                <form onSubmit={handleSearchSubmit} className="relative">
+                <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="tv" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Tv</a>
+              </li>
+              <li>
+                <a href="films" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Film</a>
+              </li>
+              <li>
+                <a href="contacts" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+              </li>
+              <li>
+              <form onSubmit={handleSearchSubmit} className="relative mt-3 md:mt-0">
                   <input
                     type="text"
                     value={searchQuery}
                     onChange={handleSearchChange}
                     placeholder="Search"
-                    className="block px-3 bg-gray-800 text-white rounded-md w-full md:w-auto md:mr-2"
+                    className="block w-full px-4 py-1 bg-gray-800 text-white rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
                   />
-                  <button type="submit" className="absolute right-2 -top-2 mt-2 mr-2 text-gray-500 hover:text-gray-300">
+                  <button type="submit" className="absolute right-3 -mt-7 text-gray-400 hover:text-white">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 4a6 6 0 100 12 6 6 0 000-12zm6 6h.01M21 21l-4.35-4.35"/>
                     </svg>
@@ -97,9 +100,9 @@ const Header = () => {
       </nav>
       <div className="bg-black">
         {filteredResults.length > 0 && (
-          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
             {filteredResults.map((film) => (
-              <SearchResults 
+              <SearchResults
                 key={film.id}
                 title={film.title}
                 posterPath={film.poster_path}
@@ -111,7 +114,7 @@ const Header = () => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
