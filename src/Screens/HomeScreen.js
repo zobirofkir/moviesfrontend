a4e6afa772d5d73084d179b02aa790ad32ad23ef
zobirofkir <zobirofkir19@gmail.com>
2024-08-11 10:07:38 +0100

    Add Adsense Code In HomeScreen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -29,6 +29,18 @@ const HomeScreen = () => {
     return () => clearInterval(interval);
   }, [film, setFeaturedMovieIndex]);
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1038335302961000";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   const fetchMovieDetail = async (id) => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_API_LINK}/api/movies/1/${id}`);
@@ -54,60 +66,69 @@ const HomeScreen = () => {
   };
 
   return (
-      <div className="bg-black text-white">
-        {film.length > 0 && !selectedMovie && (
-          <HeroSection  
-            title={film[featuredMovieIndex].title} 
-            image={`https://image.tmdb.org/t/p/original${film[featuredMovieIndex].backdrop_path}`}
-            description={film[featuredMovieIndex].overview}
-            onClick={handleShowDetails}
-          />
-        )}
-        <div className="p-2 md:p-20">
-          {selectedMovie ? (
-            <div>
-              <MovieDetail movie={selectedMovie} />
+    <div className="bg-black text-white">
+      {film.length > 0 && !selectedMovie && (
+        <HeroSection  
+          title={film[featuredMovieIndex].title} 
+          image={`https://image.tmdb.org/t/p/original${film[featuredMovieIndex].backdrop_path}`}
+          description={film[featuredMovieIndex].overview}
+          onClick={handleShowDetails}
+        />
+      )}
+      <div className="p-2 md:p-20">
+        {selectedMovie ? (
+          <div>
+            <MovieDetail movie={selectedMovie} />
+          </div>
+        ) : (
+          <>
+            <div className="mb-8">
+              <h2 className="text-xl md:text-3xl font-bold m-5">Popular Movies</h2>
+              <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                {film.map((movie) => {
+                  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+                  return (
+                    <MovieCard
+                      key={movie.id}
+                      title={movie.title}
+                      image={imageUrl}
+                      rating={movie.vote_average}
+                      onClick={() => fetchMovieDetail(movie.id)}
+                    />
+                  );
+                })}
+              </div>
             </div>
-          ) : (
-            <>
-              <div className="mb-8">
-                <h2 className="text-xl md:text-3xl font-bold m-5">Popular Movies</h2>
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                  {film.map((movie) => {
-                    const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-                    return (
-                      <MovieCard
-                        key={movie.id}
-                        title={movie.title}
-                        image={imageUrl}
-                        rating={movie.vote_average}
-                        onClick={() => fetchMovieDetail(movie.id)}  // Pass the handler here
-                      />
-                    );
-                  })}
-                </div>
-              </div>
 
-              <div className="flex justify-center items-center space-x-4 mt-8">
-                <button 
-                  onClick={handlePreviousPage} 
-                  disabled={currentPage === 1} 
-                  className="bg-gray-700 text-white px-4 py-2 rounded-md"
-                >
-                  Previous
-                </button>
-                <span className="text-xl">{currentPage}</span>
-                <button 
-                  onClick={handleNextPage} 
-                  className="bg-gray-700 text-white px-4 py-2 rounded-md"
-                >
-                  Next
-                </button>
-              </div>
-            </>
-          )}
-        </div>
+            <div className="flex justify-center items-center space-x-4 mt-8">
+              <button 
+                onClick={handlePreviousPage} 
+                disabled={currentPage === 1} 
+                className="bg-gray-700 text-white px-4 py-2 rounded-md"
+              >
+                Previous
+              </button>
+              <span className="text-xl">{currentPage}</span>
+              <button 
+                onClick={handleNextPage} 
+                className="bg-gray-700 text-white px-4 py-2 rounded-md"
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
       </div>
+      
+      {/* AdSense Ad Unit Example */}
+      <div className="my-8">
+        <ins className="adsbygoogle"
+             style={{ display: 'block' }}
+             data-ad-client="ca-pub-1038335302961000"
+             data-ad-slot="1234567890"
+             data-ad-format="auto"></ins>
+      </div>
+    </div>
   );
 };
 
